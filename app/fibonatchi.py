80f80484d8ae5d7d8f535b91562a79a0cd622dc4
Andreas Hansson <andreas.hansson@icomera.com>
2023-11-02 15:25:49 +0100

    Add logic in func for get fibonatchi sequence

--- app/fibonatchi.py
+++ app/fibonatchi.py
@@ -1,4 +1,25 @@
 
 def create_fibonatchi_values(position):
+    if int(position) <= 0:
+        raise WrongInputError("number must be above 0")
+
+    index = 0
     v = {}
-    return v
+    old = 1
+    new = 1
+    while True:
+        index += 1
+        if index == 1:
+            v["1"] = "0"
+            continue
+        elif index == 2:
+            v["2"] = "1"
+            continue
+
+        res = old + new
+        v[str(index)] = str(res)
+        old = new
+        new = res
+
+        if index >= int(position):
+            return v
