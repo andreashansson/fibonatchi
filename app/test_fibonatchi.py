e4c0888d8c9cee5468173545db696ab878e7f808
Andreas Hansson <andreas.hansson@icomera.com>
2023-11-02 15:42:08 +0100

    Add test for fibonatchi class methods

--- app/test_fibonatchi.py
+++ app/test_fibonatchi.py
@@ -1,8 +1,17 @@
 import unittest
 import fibonatchi
-
+import json
+import os
 
 class TestFibonatchi(unittest.TestCase):
+    def setUp(self):
+        data = ["5", "10"]
+        with open("skipped_test.json", "w") as json_file:
+            json.dump(data, json_file)
+
+    def tearDown(self):
+        os.remove("skipped_test.json")
+
     def test_create_fibonatchi_values(self):
         res = fibonatchi.create_fibonatchi_values("14")
         self.assertEqual(
@@ -37,4 +46,35 @@ class TestFibonatchi(unittest.TestCase):
         self.assertEqual(
             res,
             {"1": "0", "2": "1"}
+        )
+
+    def test_get_fib_val_by_fib_pos_not_skipped(self):
+        fb = fibonatchi.Fibonatchi("skipped_test.json")
+        res = fb.get_fib_val_by_fib_pos("11")
+        self.assertEqual(res, {"11": "89"})
+
+    def test_get_fib_val_by_fib_pos_is_skipped(self):
+        fb = fibonatchi.Fibonatchi("skipped_test.json")
+        res = fb.get_fib_val_by_fib_pos("10")
+        self.assertEqual(res, {"10": "skipped"})
+
+    def test_get_fib_vals_up_to_fib_pos_with_skipped(self):
+        fb = fibonatchi.Fibonatchi("skipped_test.json")
+        res = fb.get_fib_vals_up_to_fib_pos("14")
+        self.assertEqual(
+            res,
+            {
+                '1': '0',
+                '2': '1',
+                '3': '2',
+                '4': '3',
+                '6': '8',
+                '7': '13',
+                '8': '21',
+                '9': '34',
+                '11': '89',
+                '12': '144',
+                '13': '233',
+                '14': '377'
+            }
         )
